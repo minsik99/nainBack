bdf9ebeba90e4662ffdf97e4710a62ebda87e318
haelim <gpfla7375@gmail.com>
2024-06-07 15:46:49 +0900

    Member.java Error test

--- src/main/java/io/paioneer/nain/companylist/controller/CompanylistController.java
+++ src/main/java/io/paioneer/nain/companylist/controller/CompanylistController.java
@@ -4,11 +4,13 @@ import io.paioneer.nain.companylist.model.dto.CompanylistDto;
 import io.paioneer.nain.companylist.model.service.CompanylistService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,6 +27,7 @@ public class CompanylistController {
 
         List<CompanylistDto> companylistDto = companylistService.selectCompanyList(sorting);
 
+        
         return new ResponseEntity<>(companylistDto, HttpStatus.OK);
 
     }
