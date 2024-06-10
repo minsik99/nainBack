a7c2a06adddfb94fce25271df8e5577e1281c83e
DESKTOP-APA79D3\kimmi <kimminsik0726@gmail.com>
2024-06-10 12:09:24 +0900

    delete : 크롤링 관련 ENTITY/DTO 삭제

--- src/main/java/io/paioneer/nain/trend/jpa/repository/TrendRepository.java
+++
@@ -1,9 +0,0 @@
-package io.paioneer.nain.trend.jpa.repository;
-
-import io.paioneer.nain.trend.jpa.entity.TrendEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TrendRepository extends JpaRepository<TrendEntity, Long>, TrendRepositoryCustom{
-}
