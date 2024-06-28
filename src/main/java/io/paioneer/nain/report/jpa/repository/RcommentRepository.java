3351c0426ef0b7d58e5131a00cb5a5e814504e27
Jha <what1254@gmail.com>
2024-06-28 12:06:57 +0900

    24.06.28 12:06 Community 게시판 신고 중복 신고 불가능 처리, 관련 Custom Repository 작성

--- src/main/java/io/paioneer/nain/report/jpa/repository/RcommentRepository.java
+++ src/main/java/io/paioneer/nain/report/jpa/repository/RcommentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RcommentRepository extends JpaRepository<RcommentEntity, Long> {
+public interface RcommentRepository extends JpaRepository<RcommentEntity, Long>, RcommentRepositoryCustom {
 }
--- src/main/java/io/paioneer/nain/report/jpa/repository/RcommunityRepository.java
+++ src/main/java/io/paioneer/nain/report/jpa/repository/RcommunityRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RcommunityRepository extends JpaRepository<RcommunityEntity, Long> {
+public interface RcommunityRepository extends JpaRepository<RcommunityEntity, Long>, RcommunityRepositoryCustom {
+
 }
