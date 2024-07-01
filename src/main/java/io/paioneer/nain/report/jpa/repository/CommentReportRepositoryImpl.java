e6fe89441f7df242a207300191f4237b8f846914
RhysBlair <hanmanman12345@gmail.com>
2024-07-01 09:28:57 +0900

    신고리스트 계정차단 오류수정

--- src/main/java/io/paioneer/nain/report/jpa/repository/CommentReportRepositoryImpl.java
+++ src/main/java/io/paioneer/nain/report/jpa/repository/CommentReportRepositoryImpl.java
@@ -7,7 +7,6 @@ import io.paioneer.nain.member.jpa.entity.QMemberEntity;
 import io.paioneer.nain.report.jpa.entity.QRcommentEntity;
 import io.paioneer.nain.report.model.dto.CommentReportCountDto;
 import io.paioneer.nain.report.model.dto.CommentReportDto;
-import io.paioneer.nain.report.model.dto.CommunityReportCountDto;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -19,6 +18,7 @@ public class CommentReportRepositoryImpl implements CommentReportRepositoryCusto
     QRcommentEntity commentRepositoryEntity = QRcommentEntity.rcommentEntity;
     QCommentEntity commentEntity = QCommentEntity.commentEntity;
     QMemberEntity memberEntity = QMemberEntity.memberEntity;
+    QMemberEntity adminMember = new QMemberEntity("adminMember");
 
     @Override
     public List<CommentReportDto> getCommentReport() {
@@ -33,11 +33,12 @@ public class CommentReportRepositoryImpl implements CommentReportRepositoryCusto
                         commentEntity.content,
                         commentRepositoryEntity.reportType,
                         commentRepositoryEntity.handledDate,
-                        memberEntity.memberName
+                        adminMember.memberName
                 ))
                 .from(commentRepositoryEntity)
                 .leftJoin(commentEntity).on(commentRepositoryEntity.commentEntity.commentNo.eq(commentEntity.commentNo))
                 .leftJoin(memberEntity).on(commentEntity.memberEntity.memberNo.eq(memberEntity.memberNo))
+                .leftJoin(adminMember).on(commentRepositoryEntity.adminEntity.memberNo.eq(adminMember.memberNo))
                 .fetch();
     }
 
